a4e7e788a3dca0aac3882a2344d51542d6b5b5cf
Frank Hunleth <fhunleth@troodon-software.com>
2021-05-17 23:18:20 -0400

    Add Elixir 1.12 and remove support for 1.8
    This mirrors a change to vintage_net.

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule VintageNetWiFi.MixProject do
     [
       app: :vintage_net_wifi,
       version: @version,
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       test_coverage: [tool: ExCoveralls],
       start_permanent: Mix.env() == :prod,
