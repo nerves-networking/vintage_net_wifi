a3ecb630fea986fa6979e546cefbb26f17ce1c66
Frank Hunleth <fhunleth@troodon-software.com>
2026-04-08 09:36:12 -0400

    Bump deps and CI to latest
    This ignores the new credo max nesting warning rather than fix it.

--- .credo.exs
+++ .credo.exs
@@ -14,7 +14,8 @@
         {Credo.Check.Readability.LargeNumbers, only_greater_than: 86400},
         {Credo.Check.Readability.ParenthesesOnZeroArityDefs, parens: true},
         {Credo.Check.Readability.Specs, tags: []},
-        {Credo.Check.Readability.StrictModuleLayout, tags: []}
+        {Credo.Check.Readability.StrictModuleLayout, tags: []},
+        {Credo.Check.Refactor.Nesting, max_nesting: 3}
       ]
     }
   ]
